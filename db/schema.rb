4a80fb71897eea9514135ef50c7584e2e326bf1b
Mirosław Pragłowski <m@praglowski.com>
2015-01-18 14:00:13 +0100

    database schema

---
+++ db/schema.rb
@@ -0,0 +1,42 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150118114346) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_lines", force: :cascade do |t|
+    t.string  "order_uid"
+    t.integer "product_id"
+    t.string  "product_name"
+    t.integer "quantity"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "uid"
+    t.string "number"
+    t.string "customer"
+    t.string "state"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
